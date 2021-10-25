6e3c294027f4cffd2a465b906e284f75297307ca
Shana Tirtawidjaja <stirt001@ucr.edu>
2021-10-25 13:33:09 -0700

    change to single vector

--- 8puzzle.cpp
+++ 8puzzle.cpp
@@ -6,50 +6,43 @@ eightpuzzle::eightpuzzle(){ //default constructor
 void eightpuzzle::startGame(){
 	block* b = new block;
 	bool foundGoal = false;
+    nodesExpanded = 0;
+    queueNodes = 0;
 
 	//sample board
-	vector<char> top = {'1', '2', '3'};
-	vector<char> mid = {'4','5','6'};
-	vector<char> bottom = {'7', '*', '8'};
-	b->board.push_back(top);
-	b->board.push_back(mid);
-	b->board.push_back(bottom);
+
+	b->board = {'1', '2', '3', '4', '5', '6', '7', '*', '8'};
 
 	//initialize parent and children
 	b->up = nullptr;
 	b->down = nullptr;
 	b->right = nullptr;
 	b->left = nullptr;
+	b->parent = nullptr;
 
 	//prrint out puzzle 
 	outputPuzzle(b);
 	solveCheck(b->board);
 }
 void eightpuzzle::outputPuzzle(block* cur){
-	for(unsigned i = 0; i < 3; i++){ //output the puzzle line by line
-			for(unsigned j = 0; j < 3; j++){
-				cout << cur->board.at(i).at(j) << " ";
+	for(unsigned i = 0; i < 9; i++){ //output the puzzle line by line
+			cout << cur->board.at(i) << " ";
+			if((i+1) % 3 == 0 ){
+				cout << endl;
 			}
-			cout << endl;
-		}
+	}
 }
 
-bool eightpuzzle::solveCheck(vector<vector<char>> board){
-	vector<vector<char>> solution;
-	vector<char> topSol = {'1', '2', '3'};
-	vector<char> midSol = {'4','5','6'};
-	vector<char> bottomSol = {'7', '8', '*'};
-
-	solution.push_back(topSol);
-	solution.push_back(midSol);
-	solution.push_back(bottomSol);
-
+bool eightpuzzle::solveCheck(vector<char> board){
+	vector<char> solution = {'1', '2', '3', '4', '5', '6', '7', '8', '*'};
+	cout << "in check" << endl;
 	if(board == solution){
-		return true;
 		cout << "true";
+		return true;
 	}
 	else {
-		return false;
 		cout << "false";
+		return false;
+		
 	}
 }
--- 8puzzle.h
+++ 8puzzle.h
@@ -8,7 +8,8 @@
 using namespace std;
 
 struct block{ //  node with 4 children(4 directions)
-    vector<vector<char>> board; //vector with puzzle board
+//    vector<vector<char>> board; //vector with puzzle board
+    vector<char> board;
 	block* up; // child that has the blank move up
 	block* down; // child that has the blank move down
 	block* left; // child that has the blank move left
@@ -21,7 +22,10 @@ class eightpuzzle{
     block* root; 
     block* goal; // goal state
     bool foundGoal; // marks if the goal state has been found or not
-    bool solveCheck(vector<vector<char>>); // tests if the current puzzle is solved
+    int nodesExpanded; // number of nodes that have been expanded
+    int queueNodes; // number of nodes currently in queue
+  //  bool solveCheck(vector<vector<char>>); // tests if the current puzzle is solved
+  bool solveCheck(vector<char>);
     public:
     eightpuzzle();
     void startGame();
@@ -31,62 +35,6 @@ class eightpuzzle{
 //////////////////////////////////////////////////////////
 // definitions temporary
 //////////////////////////////////////////////////////////
-
-eightpuzzle::eightpuzzle(){ //default constructor
-	root = nullptr;
-}
-void eightpuzzle::startGame(){
-	block* b = new block;
-	bool foundGoal = false;
-
-	//sample board
-	vector<char> top = {'1', '2', '3'};
-	vector<char> mid = {'4','5','6'};
-	vector<char> bottom = {'7', '8', '*'};
-	b->board.push_back(top);
-	b->board.push_back(mid);
-	b->board.push_back(bottom);
-
-	//initialize parent and children
-	b->up = nullptr;
-	b->down = nullptr;
-	b->right = nullptr;
-	b->left = nullptr;
-
-	//prrint out puzzle 
-    cout << "ouput" << endl;
-	outputPuzzle(b);
-    cout << "checking" << endl;
-	solveCheck(b->board);
-}
-void eightpuzzle::outputPuzzle(block* cur){
-	for(unsigned i = 0; i < 3; i++){ //output the puzzle line by line
-			for(unsigned j = 0; j < 3; j++){
-				cout << cur->board.at(i).at(j) << " ";
-			}
-			cout << endl;
-		}
-}
-
-bool eightpuzzle::solveCheck(vector<vector<char>> board){
-	vector<vector<char>> solution;
-	vector<char> topSol = {'1', '2', '3'};
-	vector<char> midSol = {'4','5','6'};
-	vector<char> bottomSol = {'7', '8', '*'};
-
-	solution.push_back(topSol);
-	solution.push_back(midSol);
-	solution.push_back(bottomSol);
-	if(board == solution){
-        cout << "true" << endl;
-		return true;
-	}
-	else {
-        cout << "false" << endl;
-		return false;
-	}
-}
-
 
 
 #endif
